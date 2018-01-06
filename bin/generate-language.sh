488ee2746dd1606b28cabfde6ee3c2f5eb40cc89
Judzhin Miles <info@msbios.com>
2018-01-06 20:57:19 +0200

    Lazy Composer Commiter

--- bin/generate-language.sh
+++ bin/generate-language.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# COLORS
+# Reset
+Color_Off='\033[0m' # Text Reset
+
+# Regular Colors
+Red='\033[0;31m' # Red
+Green='\033[0;32m' # Green
+Yellow='\033[0;33m' # Yellow
+Purple='\033[0;35m' # Purple
+Cyan='\033[0;36m' # Cyan
+
 echo "[--------------------------------[`date +%F-%H:%M:%S`]--------------------------]"
 echo "     ____.         .___      .__    .__            _____  .__.__                 "
 echo "    |    |__ __  __| _/______|  |__ |__| ____     /     \ |__|  |   ____   ______"
